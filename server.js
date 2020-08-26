254ca4f136ff2429c732844d1c393bc188aa709b
Sergei Liddell <liddell.s@pm.me>
2020-08-25 22:26:03 -0600

    Initial commit
    * Plans implementation of Server/Express

--- server.js
+++ server.js
@@ -8,3 +8,22 @@ app.use('/products/:productId', express.static('public'));
 app.listen(port, () => {
     console.log(`server is listening on port ${port}`)
 })
+
+// Initial Commit Notes From Back-end Engineer
+/* Build required Routes from the API documentation
+                                   Express              MongoDB             MongoDB Shell
+What are the routes?      [client] ------> [server.js] ---------> [MongooseDB] ------------> [AWS]
+
+
+
+Soooooo ---> The client  will send a request for data
+Then,
+
+
+
+
+
+THOUGHTS:
+where does docker fit into this! ? Instead of e2.MicroInstances and clusters I would use docker and kubernetes to scale to 10M
+remove psuedocode before Plan implementation commit.
+*/
