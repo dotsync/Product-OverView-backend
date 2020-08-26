00da96a724bfb0e3e0e11955cf80b9d77256352e
Sergei Liddell <liddell.s@pm.me>
2020-08-26 14:21:32 -0600

    Installs dependencies

--- server/server.js
+++ server/server.js
@@ -1,18 +1,29 @@
-const express = require("express");
+/* eslint-disable no-console */
+const express = require('express');
+
 const app = express();
-const port = process.env.PORT || 8080;
+// const port = process.env.PORT || 5000;
+const port = 5000;
+// app.use(express.static('public'))
+// app.use('/products/:productId', express.static('public'));
 
-app.use(express.static('public'))
-app.use('/products/:productId', express.static('public'));
+// middle ware?
 
-app.listen(port, () => {
-    console.log(`server is listening on port ${port}`)
-})
+app.use((req, res, next) => {
+  console.log('Middleware');
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 /* OBJECTIVE: Build required Routes from the API documentation
 
-                                   Express              MongoDB             MongoDB Shell
-Routes                    [client] ------> [server.js] ---------> [MongooseDB] ------------> [AWS]
+                                   Express              MongoDB                MongoDB Shell
+Routes                    [client] ------> [server.js] ---------> [MongoDB] ------------> [AWS]
+
+TODO MATCH ENDPOINTS and VERB between frontend and routes
 
 GUIDE:
 1. Build routes with hard-coded responses.
@@ -29,6 +40,28 @@ Example: Client will request `GET /products/list` so that it can retrieve the li
 		"default_price": "140"
 	}
 ]
+
 2. Copy the above guideline for all 4 routes for the future MongoDB containers.
-3. Now make a database and connect to express
+3. Create a mongoose server for the db
+4. Now make a database and connect to express
 (ADDITIONAL STEPS MAY BE REQUIRED) */
+
+/*
+REQUEST FROM CLIENT
+<App />
+    Promise.all([
+      axios.get(`/reviews/${productId}/meta`),
+      axios.get(`/products/${productId}`),
+      axios.get(`/products/${productId}/styles`)
+    ])
+*/
+
+// route 1
+
+// route 2
+// route 3
+// route 4
+
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}`);
+});
