54d7ef238bee3851823c30d89f7d2ef4a86821d3
Sergei Liddell <liddell.s@pm.me>
2020-08-26 23:08:27 -0600

    Adds a controller to find productID

--- server/routes/product_id.js
+++ server/routes/product_id.js
@@ -3,37 +3,11 @@
 /* eslint-disable eqeqeq */
 const express = require('express');
 
-const HttpError = require('../models/http-error.js');
+const productsControllers = require('../controllers/products-controllers.js');
 
 const router = express.Router();
 
-const DUMMY_DATA = [{
-  id: 11,
-  name: 'Air Minis 250',
-  slogan: 'Full court support',
-  description: 'This optimized air cushion pocket reduces impact but keeps a perfect balance underfoot.',
-  category: 'Basketball Shoes',
-  default_price: '0',
-  features: [
-    {
-      feature: 'Sole',
-      value: 'Rubber',
-    },
-    {
-      feature: 'Material',
-      value: 'FullControlSkin',
-    },
-  ],
-}];
 // hardcoded id: 11
-router.get('/:product_id', (req, res, next) => {
-  // console.log(req.params);
-  const productId = req.params.product_id;
-  const product = DUMMY_DATA.find((p) => p.id == productId);
-  if (!product) {
-    throw new HttpError('Could not find product with the given id');
-  }
-  res.json({ product });
-});
+router.get('/:product_id', productsControllers.getProductByID);
 
 module.exports = router;
